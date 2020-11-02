4fbd6bcfc70bd40791202155d2263c14e83d7545
Daniel Leushuis <leushuis551@hotmail.com>
2020-11-02 22:19:16 +0100

    updated all libraries + webrtc library

--- src/app/features/main/main.page.ts
+++ src/app/features/main/main.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
