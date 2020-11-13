d1d1d2dee6b4319b0f2592342b27a2fa13cec67c
PimKoster1 <PimKoster1@github.com>
2020-11-13 14:19:02 +0100

    fixed build

--- src/app/features/home/home.page.ts
+++ src/app/features/home/home.page.ts
@@ -1,28 +1,9 @@
 import { Component } from "@angular/core";
-import {ConfigProvider} from "../../providers/config/configProvider";
 
 @Component({
     templateUrl: "home.page.html",
     styleUrls: ["home.page.scss"]
 })
 export class HomePageComponent {
-<<<<<<< HEAD
-=======
-    showExternalInstruction$: any;
-    loginType$: any;
-    appName: string;
-    appDesc: string;
-
-    constructor(
-        private configProvider: ConfigProvider
-    ) {
-        this.configProvider.getConfig();
-    }
-
-    ngOnInit() {
-        this.appName = this.configProvider.appName;
-        this.appDesc = this.configProvider.appDescription;
-    }
->>>>>>> 96b785443ec48c3b90fcebbbb7ecb9ab19068f18
 
 }
